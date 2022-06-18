509aee1f63c94a5a0f1393f5ee431d994e1c7cc5
Christopher <kipherkode@gmail.com>
2022-06-18 16:03:43 -0400

    Launch 🚀

---
+++ main.py
@@ -0,0 +1,33 @@
+from cmath import exp
+from re import L
+import pyautogui
+import time
+
+input('')
+
+
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionString = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print(positionString, end='')
+        print('\b' * len(positionString), end='', flush=True)
+except KeyboardInterrupt:
+    print('\n')
+
+def bee():
+    time.sleep(5)
+    pyautogui.position(x,y)
+    pyautogui.click(x,y)
+    file = open('BeeMovieScript.txt', 'r')
+    try:
+        for line in file:
+            for char in line:
+                pyautogui.typewrite(char)
+    except KeyboardInterrupt:
+        print('Bee Stoped')
+
+explore = input(':')
+
+if explore == 'bee':
+    bee()
